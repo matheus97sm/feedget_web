93197dc395c6ebf1b55e1e8545e85b78f1a63f43
matheus97sm <matheus97sm@gmail.com>
2022-05-03 01:03:30 -0300

    Creating the popup button

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,12 +1,23 @@
+import { Popover } from '@headlessui/react';
+
 import { ChatTeardropDots } from 'phosphor-react';
 
 export function Widget() {
   return (
-    <button
-      className="bg-violet-500 rounded-full px-3 h-12 text-white"
-      type="button"
-    >
-      <ChatTeardropDots className="w-6 h-6" />
-    </button>
+    <Popover className="absolute bottom-4 right-4">
+      <Popover.Panel>ola</Popover.Panel>
+
+      <Popover.Button
+        className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
+        type="button"
+      >
+        <ChatTeardropDots className="w-6 h-6" />
+
+        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+          <span className="pl-2" />
+          Feedback
+        </span>
+      </Popover.Button>
+    </Popover>
   );
 }
